6a22c91c5483e1d9e6cdd94e8f637eb1e19bfa0f
Shubham Mishra <you@example.com>
2019-02-07 11:57:34 -0500

    Day 1 learning

---
+++ usingVariables.R
@@ -0,0 +1,21 @@
+A <- 10
+B <- 5
+
+C <- A+B
+C
+
+#variable1
+var1 <- 2.5
+#variable2
+var2 <- 4
+
+result <- var1 / var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "hello"
+name <- "Bob"
+message <- paste(greeting,name)
+message
